dff4cf664fc3b86adf0d3e1e69ebbd4a2aae49aa
Gabriel Seneca <86847957+GabrielSeneca@users.noreply.github.com>
2022-04-08 17:21:23 -0300

    Agregar archivos de proyecto.

---
+++ Operaciones/Calcular.cs
@@ -0,0 +1,23 @@
+﻿namespace Operaciones
+{
+    public class Calcular
+    {
+        
+        public static double Suma(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Resta(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multi(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
